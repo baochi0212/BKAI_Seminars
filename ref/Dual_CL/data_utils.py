dd8c4185f6dd2417465117f8a51a68c7a6202a6e
baochi0212 <chideptraika29@gmail.com>
2022-12-13 13:04:09 +0700

    dual CL

--- ref/Dual_CL/data_utils.py
+++ ref/Dual_CL/data_utils.py
@@ -32,7 +32,7 @@ class MyDataset(Dataset):
         dataset = list()
         for data in raw_data:
             tokens = data['text'].lower().split(' ')
-            if data['label'] not in label_dict:
+            if '[' + data['label'] + ']' not in label_dict:
                 label_id = 0
             else:
                 label_id = label_dict[data['label']]
