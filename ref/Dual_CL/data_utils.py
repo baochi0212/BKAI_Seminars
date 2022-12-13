2be9350d274cc76252a43d4006be3cfaf1db4cb5
baochi0212 <chideptraika29@gmail.com>
2022-12-14 00:17:01 +0700

    ...

--- ref/Dual_CL/data_utils.py
+++ ref/Dual_CL/data_utils.py
@@ -122,11 +122,9 @@ def load_data(dataset, data_dir, tokenizer, train_batch_size, test_batch_size, m
         test_data = json.load(open(os.path.join(data_dir, 'phoATIS_Test.json'), 'r', encoding='utf-8'))
         label_dict = text2dict('phoATIS_label.txt')
     elif dataset == 'uit_nlp':
-        train_data = json.load(open(os.path.join(data_dir, 'phoATIS_Train.json'), 'r', encoding='utf-8'))
-        test_data = json.load(open(os.path.join(data_dir, 'phoATIS_Test.json'), 'r', encoding='utf-8'))
-        
-        train_data = json.load(open(os.path.join(data_dir, 'phoATIS_Train.json'), 'r', encoding='utf-8'))
-        test_data = json.load(open(os.path.join(data_dir, 'phoATIS_Test.json'), 'r', encoding='utf-8'))
+        train_data = json.load(open(os.path.join(data_dir, 'uit-nlp_Train.json'), 'r', encoding='utf-8'))
+        test_data = json.load(open(os.path.join(data_dir, 'uit-nlp_Test.json'), 'r', encoding='utf-8'))
+        label_dict = dict([(key, key) for key in [0, 1, 2]])
 
     else:
         raise ValueError('unknown dataset')
