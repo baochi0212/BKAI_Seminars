c10adb1c9ad9accbb7a85135bc2a3ecd641e594c
baochi0212 <chideptraika29@gmail.com>
2022-12-14 17:25:34 +0700

    ...

--- ref/Dual_CL/data_utils.py
+++ ref/Dual_CL/data_utils.py
@@ -119,7 +119,6 @@ def load_data(dataset, data_dir, tokenizer, train_batch_size, test_batch_size, m
         train_data = train_data[:int(len(train_data)//10)+1]
         
         label_dict = {'positive': 0, 'negative': 1}
-
     elif dataset == 'trec':
         train_data = json.load(open(os.path.join(data_dir, 'TREC_Train.json'), 'r', encoding='utf-8'))
         test_data = json.load(open(os.path.join(data_dir, 'TREC_Test.json'), 'r', encoding='utf-8'))
