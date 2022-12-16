93fbc638ca902ca4d6814ab26c59db6db0359a3e
baochi0212 <chideptraika29@gmail.com>
2022-12-16 16:13:16 +0700

    ...

--- ref/Dual_CL/loss_func.py
+++ ref/Dual_CL/loss_func.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class CELoss(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.xent_loss = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 0.5, 1]))
+        self.xent_loss = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 0.5, 1]).to(device))
 
     def forward(self, outputs, targets):
         return self.xent_loss(outputs['predicts'], targets)
