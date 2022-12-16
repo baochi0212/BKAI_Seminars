388df30a5b44998b5b65a0a5c70eecf3ff68ab2a
baochi0212 <chideptraika29@gmail.com>
2022-12-16 14:54:17 +0700

    ...

--- ref/Dual_CL/main.py
+++ ref/Dual_CL/main.py
@@ -87,7 +87,7 @@ class Instructor:
                 # print(targets) #for verify sanity
                 n_correct += (torch.argmax(outputs['predicts'], -1) == targets).sum().item()
                 y_pred += torch.argmax(outputs['predicts'], -1).cpu().numpy().reshape(-1).tolist()
-                y_true += targets.numpy().reshape(-1).cput().tolist()
+                y_true += targets.cpu().numpy().reshape(-1).tolist()
                 n_test += targets.size(0)
         print(classification_report(y_true, y_pred))
         return test_loss / n_test, n_correct / n_test
