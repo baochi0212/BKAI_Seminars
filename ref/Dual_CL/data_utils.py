954e9f4602b000a76b6a5e97827d5314a09d274e
baochi0212 <chideptraika29@gmail.com>
2022-12-16 15:44:25 +0700

    ...

--- ref/Dual_CL/data_utils.py
+++ ref/Dual_CL/data_utils.py
@@ -183,4 +183,4 @@ if __name__ == '__main__':
     # # #         print(word)
     # # #         break
 
-    dataset2json(filename, proportion=1, max_sample=500)
+    dataset2json(filename, proportion=1, max_sample=1000)
