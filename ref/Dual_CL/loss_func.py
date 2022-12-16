d6d0aa25464581347ba28de046aa113257e4fff8
baochi0212 <chideptraika29@gmail.com>
2022-12-16 16:10:59 +0700

    ...

--- ref/Dual_CL/loss_func.py
+++ ref/Dual_CL/loss_func.py
@@ -7,7 +7,7 @@ class CELoss(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.xent_loss = nn.CrossEntropyLoss(weight=[0.5, 0.5, 1])
+        self.xent_loss = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 0.5, 1]))
 
     def forward(self, outputs, targets):
         return self.xent_loss(outputs['predicts'], targets)
